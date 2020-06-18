dda63a6f7b72def23d763ee19ce2b8245c65484b
Luca Bellenghi <lucabel@gmail.com>
2020-06-18 17:50:36 +0200

    fix templates for rss block

--- src/components/DesignTheme/Blocks/RssBlock/CardWithoutImageRssTemplate.jsx
+++ src/components/DesignTheme/Blocks/RssBlock/CardWithoutImageRssTemplate.jsx
@@ -11,6 +11,7 @@ import {
   CardCategory,
   CardReadMore,
 } from 'design-react-kit/dist/design-react-kit';
+import cx from 'classnames';
 
 const messages = defineMessages({
   readMore: { id: 'rss_read_more', defaultMessage: 'Read more' },
@@ -20,40 +21,51 @@ const messages = defineMessages({
   },
 });
 
-const CardWithoutImageRssTemplate = ({ items = [] }) => {
+const CardWithoutImageRssTemplate = ({ items = [], isEditMode }) => {
   const intl = useIntl();
 
   return (
-    <div className="row">
-      {items?.length > 0 ? (
-        items.map(item => (
-          <div className="col-12 col-lg-3">
-            <Card noWrapper={false} tag="div">
-              <CardBody tag="div">
-                <CardCategory date={moment(item.pubDate).format('DD-MMM-Y')}>
-                  {item.categories?.length > 0 ? item.categories[0]._ : ''}
-                </CardCategory>
-                <CardTitle className="big-heading" tag="h5">
-                  {item.title}
-                </CardTitle>
-                <CardText tag="p" className="text-serif">
-                  {item.contentSnippet}
-                </CardText>
-              </CardBody>
-              <CardReadMore
-                iconName="it-arrow-right"
-                tag="a"
-                href={item?.link}
-                text={intl.formatMessage(messages.readMore)}
-              />
-            </Card>
+    <div className={cx('', { 'public-ui': isEditMode })}>
+      <div className="row">
+        {items?.length > 0 ? (
+          items.map((item) => (
+            <div className="col-12 col-lg-3 mb-3" key={item.title}>
+              <Card className="card-bg" noWrapper={false} tag="div">
+                <CardBody tag="div">
+                  <div className="category-top">
+                    {item?.categories?.length > 0 && item.categories[0]._ ? (
+                      <>
+                        <span className="category">{item.categories[0]._}</span>
+                        <span className="mx-1">&mdash;</span>
+                      </>
+                    ) : (
+                      ''
+                    )}
+                    <span>{moment(item.pubDate).format('DD-MMM-Y')}</span>
+                  </div>
+                  <CardTitle className="big-heading" tag="h5">
+                    {item.title}
+                  </CardTitle>
+                  <CardText tag="p" className="text-serif">
+                    {item.contentSnippet}
+                  </CardText>
+                </CardBody>
+                <CardReadMore
+                  iconName="it-arrow-right"
+                  className="ml-4"
+                  tag="a"
+                  href={item?.link}
+                  text={intl.formatMessage(messages.readMore)}
+                />
+              </Card>
+            </div>
+          ))
+        ) : (
+          <div className="no-rss-feed-results">
+            {intl.formatMessage(messages.noResults)}
           </div>
-        ))
-      ) : (
-        <div className="no-rss-feed-results">
-          {intl.formatMessage(messages.noResults)}
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
