bc1c47bfc7a30e4261bb87e9bcd6c9250bb71d0a
Giulia Ghisini <giulia.ghisini@redturtle.it>
2022-05-03 10:08:59 +0200

    debug: debug cookie

--- src/customizations/volto/helpers/Cookies/cookies.js
+++ src/customizations/volto/helpers/Cookies/cookies.js
@@ -3,8 +3,8 @@ import config from '@plone/volto/registry';
 export const getCookieOptions = (options = {}) => {
   const { path = '/', secure = false, ...otherOptions } = options;
   let secureOption = secure;
-  console.log(options);
 
+  debugger;
   try {
     if (secureOption === undefined || secureOption === null) {
       const protocol = window?.location?.protocol ?? 'http';
@@ -13,15 +13,6 @@ export const getCookieOptions = (options = {}) => {
   } catch (e) {
     //window is not defined. It's ssr and we use 'secure' option passed from param
   }
-
-  console.log(options, secureOption, {
-    path: path,
-    expires: new Date(
-      new Date().getTime() + config.settings.cookieExpires * 1000,
-    ),
-    secure: secureOption,
-    ...otherOptions,
-  });
 
   return {
     path: path,
