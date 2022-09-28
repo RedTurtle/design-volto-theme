ce0ffd3f4fb19b270a3a356b0659b5fc99c065c2
Wagner Trezub <wagner.trezub@redturtle.it>
2022-09-28 15:24:38 +0200

    feat: react-highlight-words implementation inside general Search page results

--- src/components/ItaliaTheme/Search/ResultItem.jsx
+++ src/components/ItaliaTheme/Search/ResultItem.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import cx from 'classnames';
 import { Card, CardBody } from 'design-react-kit/dist/design-react-kit';
 import { UniversalLink } from '@plone/volto/components';
+import Highlighter from 'react-highlight-words';
 
-const ResultItem = ({ item, index, section }) => {
+const ResultItem = ({ item, index, section, searchableText }) => {
+  const filteredWords = searchableText.split(' ');
+
   return (
     <Card
       teaser
@@ -15,9 +18,23 @@ const ResultItem = ({ item, index, section }) => {
       <CardBody>
         {section}
         <h4 className="card-title">
-          <UniversalLink item={item}>{item.title}</UniversalLink>
+          <UniversalLink item={item}>
+            <Highlighter
+              highlightClassName="highlighted-text"
+              searchWords={filteredWords}
+              autoEscape={true}
+              textToHighlight={item.title}
+            />
+          </UniversalLink>
         </h4>
-        <p className="card-text">{item.description}</p>
+        <p className="card-text">
+          <Highlighter
+            highlightClassName="highlighted-text"
+            searchWords={searchableText.split(' ')}
+            autoEscape={true}
+            textToHighlight={item.description}
+          />
+        </p>
       </CardBody>
     </Card>
   );
